517a584024480d5bb5e7d8e14f4a3fadb4b0d220
Shibu TN <tnshibu@yahoo.com>
2019-10-06 00:27:01 +0530

    a

--- src/main/java/Folder_Delete.java
+++ src/main/java/Folder_Delete.java
@@ -15,20 +15,8 @@ public class Folder_Delete {
   private static String repoDir = "";
   //-----------------------------------------------------------------------------------------------------------------------
   public static void main(String[] args) throws Exception {
-    //if(args != null ) {
-    //    System.out.println("args.length = "+args.length);
-    //}
     List<String> argList = new ArrayList<String>();
     Collections.addAll(argList, args);
-    //System.out.println(argList.size() +":"+argList);
-    
-    //if(argList != null && argList.size() >= 1 ) {
-    //    if(argList.get(0).trim().equalsIgnoreCase("-f")) {
-    //        forceFlag = true;
-    //        argList.remove(0);
-    //    }
-    //}
-    //System.out.println(argList.size() +":"+argList);
 
     if(argList != null && argList.size() >= 1 ) {
         SOURCE_BASE_FOLDER = argList.get(0);
@@ -37,15 +25,13 @@ public class Folder_Delete {
     if(SOURCE_BASE_FOLDER.endsWith("\\")) {
         SOURCE_BASE_FOLDER = SOURCE_BASE_FOLDER.substring(0,SOURCE_BASE_FOLDER.length()-1);
     }
-    //SOURCE_BASE_FOLDER ="D:\\zShibu\\0_from_TRC\\00-shibu\\fin\\1";
     //System.out.println("SOURCE_BASE_FOLDER ='"+SOURCE_BASE_FOLDER+"'");
-    //System.out.println("forceFlag ="+forceFlag);
     File sourceFolder = new File(SOURCE_BASE_FOLDER);
     if(!sourceFolder.exists()) {
         System.out.println("Folder "+SOURCE_BASE_FOLDER + " does not exist. Exiting");
         System.exit(-1);
     }
-    //if(true)System.exit(-1);
+
     sourceFileList = FileUtil.getFileListFromFolder(SOURCE_BASE_FOLDER);
     String propertyFilePath = Util.locatePropertiesFile();
     propertiesMap = PropertiesLoader.loadToHashMap(propertyFilePath);
@@ -60,8 +46,12 @@ public class Folder_Delete {
 			System.out.println("FILE_NOT_FOUND:"+fileName);
             continue;
         } else {
-            file.delete();
-            System.out.println(fileName);
+            try {
+                file.delete();
+                System.out.println("DELETE:"+fileName);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
   }
